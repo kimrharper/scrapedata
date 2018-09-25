7c46193ed645b6985dddc4c774c52722680ff613
kimrharper <kimrharper@gmail.com>
2018-09-25 15:11:09 -0700

    completed scraper for chinese images minus char input list

--- decoder.py
+++ decoder.py
@@ -1,43 +1,55 @@
-import re
-# import chardet
-import sys
+# import re
+# # # import chardet
+# # import sys
+# #
+# #
+# # # https://www.yellowbridge.com/chinese/charsearch.php?zi=大
+# # # https://www.yellowbridge.com/chinese/charsearch.php?zi=%E5%8d%83
+# #
+# # # char = ['大','小','一','二','三','十','百','千','多','不']
+# # # big5char = ['A46A','A470']
+# # # id = ['43','49']
+# # #
+# # #
+# # #
+# # # print(sys.stdin.encoding)
+# # # for c in char:
+# # #     encoded = c.encode('utf-8')
+# # #     print(chardet.detect((encoded)))
+# # #     print(encoded)
+# #
+# # # print(big5char)
+# # import codecs
+# #
+# #
+# # char = ['大','小']
+# # s = char[1].encode('hkscs').hex()
+# # print(s)
+# #
+# # print(bytes(s))
+# # print(s.decode('big5hkscs'))
+# #
+# # print(bytearray.fromhex(b'a4j'))
+# #
+# # id = ['43','49']
+# # era = ['甲骨文','金文','楚系文字']
+# # l=[]
+# # #
+# # # for i in range(len(char)):
+# # #     for e in era:
+# # #         l.append("http://char.iis.sinica.edu.tw/Search/YiChar_SQL.aspx?char={}&word={}&font={}".format(id[i],char[i],e))
+# # #
+# # # for i in l:
+# # #     print(i)
 
+# str1 = 'hello'
+# print(str1[2:])
 
-# https://www.yellowbridge.com/chinese/charsearch.php?zi=大
-# https://www.yellowbridge.com/chinese/charsearch.php?zi=%E5%8d%83
+l = [[1, 2], [2, 3], [4, 5]]
 
-# char = ['大','小','一','二','三','十','百','千','多','不']
-# big5char = ['A46A','A470']
-# id = ['43','49']
-#
-#
-#
-# print(sys.stdin.encoding)
-# for c in char:
-#     encoded = c.encode('utf-8')
-#     print(chardet.detect((encoded)))
-#     print(encoded)
-
-# print(big5char)
-import codecs
-
-
-char = ['大','小']
-s = char[1].encode('hkscs').hex()
-print(s)
-
-print(bytes(s))
-print(s.decode('big5hkscs'))
-
-print(bytearray.fromhex(b'a4j'))
-
-id = ['43','49']
-era = ['甲骨文','金文','楚系文字']
-l=[]
-#
-# for i in range(len(char)):
-#     for e in era:
-#         l.append("http://char.iis.sinica.edu.tw/Search/YiChar_SQL.aspx?char={}&word={}&font={}".format(id[i],char[i],e))
-#
-# for i in l:
-#     print(i)
+out = open('out.csv', 'w')
+for row in l:
+    for column in row:
+        out.write('%d,' % column)
+    out.write('\n')
+out.close()
--- scraper/tutorial/spiders/char_spider.py
+++ scraper/tutorial/spiders/char_spider.py
@@ -1,30 +1,69 @@
-# from tutorial.items import MyItem
-# import scrapy
-# import time
-#
-# # char = ['大','小','一','二','三','十','百','千','多','不']
-#
-# # id = ['43','49']
-#
-# char = ['大','小']
-# id = ['43','49']
-# big5 = ['A46A','A470']
-# era = ['甲骨文','金文','楚系文字']
-# era_english = ['oracle','jinwen','chuxi']
-#
-#
-#
-# class CharSpider(scrapy.Spider):
-#     name = "chars"
-#
-#     def start_requests(self):
-#         url = 'http://char.iis.sinica.edu.tw/Search/char_SQL.aspx?char=A464&type=0'
-#         yield scrapy.Request(url=url)
-#
-#     def parse(self, response):
-#         html = response.xpath
-#         img = response.xpath("//img[@name='charImg']").extract()
-#         print(img)
-#         idnum = response.xpath("//input[@type='hidden' and @name='char']/@value").extract()
-#         print(idnum)
-#         input('')
+from tutorial.items import MyItem
+import scrapy
+import time
+
+char = ['大','小']
+era,era_english = ['甲骨文','金文','楚系文字'],['oracle','jinwen','chuxi']
+
+def digit_space(val):
+    if val < 10:
+        return '00'+str(val)
+    elif (val > 9) and (val < 100):
+        return '0'+str(val)
+    else:
+        return str(val)
+
+class CharSpider(scrapy.Spider):
+    name = "chars"
+
+    def start_requests(self):
+        for c in char:
+            big5= c.encode('hkscs').hex()
+            url = 'http://char.iis.sinica.edu.tw/Search/char_SQL.aspx?char={}&type=0'.format(big5)
+            print(url)
+            yield scrapy.Request(url=url,meta={'char':c,'big5':big5})
+
+    def parse(self, response):
+        meta = response.meta
+        # img = response.xpath("//img[@name='charImg']/@src").extract_first()[2:]
+
+        meta['id'] = idnum = response.xpath("//input[@type='hidden' and @name='char']/@value").extract()[0]
+        for u,e in enumerate(era):
+            time.sleep(3)
+            url = ("http://char.iis.sinica.edu.tw/Search/YiChar_SQL.aspx?char={}&word={}&font={}".format(idnum,
+                                                                                                         meta['char'],
+                                                                                                         era[u]))
+            meta['era'] = era[u]
+            meta['era_english'] = era_english[u]
+            yield scrapy.Request(url=url,callback=self.parse_images,meta=meta)
+
+    def parse_images(self, response):
+        i = 0
+        item = MyItem()
+
+        # code for original image [doesntwork]
+        # item['image_urls'] = 'http://char.iis.sinica.edu.tw/'+ response.meta['orig_pic']
+        # item['image_name'] = 'acs/' + response.meta['big5'] + '/' + response.meta['big5']+'.gif'
+        # yield item
+        char_meta = []
+
+        for elem in response.xpath("//img"):
+            nmeta = response.meta
+            time.sleep(3)
+            img_url = elem.xpath("@src").extract_first()
+            img_url = 'http://char.iis.sinica.edu.tw' + img_url
+            mname = 'acs/' + nmeta['big5'] + '/' + nmeta['big5'] + '-' + nmeta['era_english'] +'_'+ digit_space(i) + '.gif'
+            item['image_urls'] = img_url
+            item['image_name'] = mname
+            char_meta.append([nmeta['char'],nmeta['big5'],nmeta['id'],nmeta['era_english'],nmeta['era'],mname])
+
+            i += 1
+            yield item
+
+        with open('chars_data.csv', 'a') as c_data:
+            for row in char_meta:
+                for column in row:
+                    c_data.write(column + ',')
+                c_data.write('\n')
+        c_data.close()
+
